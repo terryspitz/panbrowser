bfed7dcdb2a99b6efb8774b9f3443487564f9e47
terryspitz_cp <SND\terryspitz_cp>
2009-12-29 03:03:43 -0800

    didn't checkin properly, so lots of enhancements: 3D support size/rotate/translate sliders play buttons

--- PanBrowser2/PanWindow.xaml.cs
+++ PanBrowser2/PanWindow.xaml.cs
@@ -11,6 +11,8 @@ using Microsoft.Win32;
 using System.IO;
 using System.Windows.Markup;
 using System.Xml;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.Animation;
 
 namespace Terry
 {
@@ -24,20 +26,27 @@ namespace Terry
         protected ComboBox transformCombo1;
 
         //a delegate to hold the simple pan function
-        public FastFunc<Pan.Point, Pan.Color> myImageFn;
-        public int myRequestNumber = 0;
+        protected FastFunc<Pan.Point, Pan.Color> myImageFn;
+        protected int myRequestNumber = 0;
+        static protected int minstep = 4;        //stored step size to start drawing with
 
-        public delegate void ShowStatus(string text);
-        public ShowStatus StatusEvent { get; set; }
-        private PanWrapper PanWrapper = new PanWrapper();
-        public List<ObservableCollection<SliderAttribute>> myAttributes = new List<ObservableCollection<SliderAttribute>>();
-        protected SliderDouble sizeSlider = new SliderDouble("Size", -10, 10, 0);
+        protected delegate void ShowStatus(string text);
+        protected ShowStatus StatusEvent { get; set; }
+        protected PanWrapper panWrapper = new PanWrapper();
+        protected List<ObservableCollection<SliderAttribute>> myAttributes = new List<ObservableCollection<SliderAttribute>>();
+        //protected SliderDouble sizeSlider = new SliderDouble("Size", -10, 10, 0);
+        protected Slider[] transformStandardSliders = new Slider[4];
 
-        private class Request
+        protected BrillWpf.BrillDockPanel brill = null;
+        protected BrillRenderer brillRenderer;
+
+        protected class Request
         {
             public int number;
             public PanBrowser _this;
-            public int width, height;
+            public int width, height, scale;
+            public delegate bool IsCancelled(int reqno);
+            public IsCancelled cancelCallback;
         }
 
         public PanBrowser()
@@ -51,35 +60,40 @@ namespace Terry
             imageSliderList.ItemsSource = imageAttributes;
             myAttributes.Add(imageAttributes);
 
+            //clone the basic image combo and controls to create the transforms combo/controls
             ObservableCollection<SliderAttribute> transformAttributes = new ObservableCollection<SliderAttribute>();
             FrameworkElement transform1 = CloneModel(imagecontrols);
             designerPanel.Children.Add(transform1);
             transformCombo1 = (ComboBox)transform1.FindName("transformCombo1");
             transformCombo1.SelectionChanged += TransformCombo1_SelectionChanged;
+            transformCombo1.ItemsSource = panWrapper.Transforms;
             ((ListView)transform1.FindName("transformSliderList")).ItemsSource = transformAttributes;
             myAttributes.Add(transformAttributes);
 
-            //make sure this is after we've clones the imagecontrols (above)
-            imageAttributes.Add(sizeSlider);
-            foreach (string image in PanWrapper.Images)
-                imageCombo1.Items.Add(image);
-            
-            foreach (string transform in PanWrapper.Transforms)
-                transformCombo1.Items.Add(transform);
+            transformStandardSliders[0] = (Slider)transform1.FindName("transformSizeSlider");
+            transformStandardSliders[1] = (Slider)transform1.FindName("transformRotateSlider");
+            transformStandardSliders[2] = (Slider)transform1.FindName("transformXSlider");
+            transformStandardSliders[3] = (Slider)transform1.FindName("transformYSlider");
+            foreach (Slider s in transformStandardSliders)
+                s.ValueChanged += Slider_ValueChanged;
+
+            //after cloning
+            panWrapper.Images.Add("Brill3D");
+            imageCombo1.ItemsSource = panWrapper.Images;
 
             RegistryKey k = Registry.CurrentUser.OpenSubKey("Software\\Terry\\PanBrowser", true);
             if (k != null)
             {
-                imageCombo1.SelectedValue = k.GetValue("image", PanWrapper.Images[0]);
-                transformCombo1.SelectedValue = k.GetValue("transform", PanWrapper.Transforms[0]);
+                imageCombo1.SelectedValue = k.GetValue("image", panWrapper.Images[0]);
+                transformCombo1.SelectedValue = k.GetValue("transform", panWrapper.Transforms[0]);
             }
             else
             {
-                imageCombo1.SelectedIndex = 0;
-                transformCombo1.SelectedIndex = 0;
+                imageCombo1.SelectedValue = "Terrys.bumpSwirl";
+                transformCombo1.SelectedValue = "Terrys.star2";
             }
 
-            Console.WriteLine(string.Format("{0} images; {1} transforms", PanWrapper.Images.Count, PanWrapper.Transforms.Count));
+            Console.WriteLine(string.Format("{0} images; {1} transforms", panWrapper.Images.Count, panWrapper.Transforms.Count));
         }
 
         
@@ -91,66 +105,96 @@ namespace Terry
             r.number = myRequestNumber;
             r.width = (int)imagePanel.ActualWidth;
             r.height = (int)imagePanel.ActualHeight;
+            r.scale = 0;// (int)(imageSizeSlider.Value * 2) - 10;
+            r.cancelCallback = IsCancelled;
+            Draw(minstep-1, r.number, r.width, r.height, r.scale);  //do a quick one in this thread before returning
 #if !SINGLETHREAD
             ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), r);
 #else
             ThreadProc(r);
 #endif
         }
+
+        bool IsCancelled(int r)
+        {
+            return r != myRequestNumber;
+        }
+
         static void ThreadProc(Object stateInfo)
         {
             Request r = (Request)stateInfo;
-            for (int step = 4; step >= -1; step--)
+            if (r.cancelCallback(r.number))
+                return;
+            //iterate through successively higher resolutions, -1 being four bitmap pixes per screen pixel for antialiasing
+            //remember the step to start with that's fast enough for recalculation e.g. resizing operations
+            for (int step = minstep; step >= -1; step--)    
             {
-                r._this.Draw(step, r.number, r.width, r.height);
+                int elapsed = r._this.Draw(step, r.number, r.width, r.height, r.scale);
                 if (r.number != r._this.myRequestNumber)
                     break;
+                if (elapsed < 50 && minstep>step)  //assume 200ms is quick enough not to notice
+                    minstep = step;
             }
         }
 
-        protected void Draw(int step, int requestNo, int Width, int Height)
+        protected int Draw(int step, int requestNo, int Width, int Height, int scale)
         {
             try
             {
                 Tuple<byte[], int, int, PixelFormat, int, int> result = null;
                 if (myImageFn != null)
                 {
-                    result = DrawImage.drawImageCol(myImageFn, Width, Height, step, sizeSlider.Val);
+                    result = DrawImage.drawImageCol(myImageFn, Width, Height, step, scale);
                 }
                 
-                if (requestNo != myRequestNumber)   //don't update if new request has been received
-                    return;
+                if (IsCancelled(requestNo))   //don't update if new request has been received
+                    return 0;
 
                 if (result != null && result.Item1 != null && result.Item1.Length>0)
                 {
                     int pixels;
                     lock (result.Item1)
                     {
-                        bitmap.Dispatcher.Invoke(
-                            (Action<Tuple<byte[], int, int, PixelFormat, int, int>>)((Tuple<byte[], int, int, PixelFormat, int, int> request) => 
-                                { 
-                                    bitmap.Source = BitmapSource.Create(result.Item2, result.Item3, 96.0, 96.0, result.Item4, null, result.Item1, result.Item5); 
-                                }
-                                ), result);
+                        if (bitmap.Dispatcher.Thread == Thread.CurrentThread)
+                        {
+                            bitmap.Source = BitmapSource.Create(result.Item2, result.Item3, 96.0, 96.0, result.Item4, null, result.Item1, result.Item5);
+                        }
+                        else
+                        {
+                            // dispatch to Main thread
+                            bitmap.Dispatcher.Invoke(
+                                (Action<Tuple<byte[], int, int, PixelFormat, int, int>>)((Tuple<byte[], int, int, PixelFormat, int, int> request) =>
+                                    {
+                                        bitmap.Source = BitmapSource.Create(result.Item2, result.Item3, 96.0, 96.0, result.Item4, null, result.Item1, result.Item5);
+                                    }
+                                    ), result);
+                        }
                         pixels = result.Item2* result.Item3;
                     }
-                    long elapsed = Math.Max(1, result.Item6);
-                    string status= String.Format("Drew {0} pixels in {1} ms\n{2} pixels/ms, {3} frames/s",
-                        pixels, elapsed.ToString("n0"),
-                        (pixels / elapsed).ToString("n0"), (1000d / elapsed).ToString("n2"));
+                    int elapsed = Math.Max(1, result.Item6);
+                    string status= String.Format("{5}: Drew {0} pixels with step \t{4} in \t{1} ms\t{2} pixels/ms\t{3} frames/s",
+                        pixels, elapsed.ToString("n0")
+                        , (pixels / elapsed).ToString("n0")
+                        , (1000d / elapsed).ToString("n2")
+                        ,step
+                        ,"" //,DateTime.Now.ToString("HH:mm:ss.fff")
+                        );
                     Console.WriteLine(status);
                     //StatusEvent(status);
+                    return elapsed;
                 }
                 else
                 {
                     //if(this.InvokeRequired)
                     //StatusEvent("null image");
+                    return 0;
                 }
             }
             catch (Exception ex)
             {
                 if (StatusEvent != null)
                     StatusEvent(ex.ToString());
+                return 0;
             }
 
         }
@@ -161,27 +205,11 @@ namespace Terry
             //gridXaml.Replace("<Label>Image:</Label>", "<Label>Transform:</Label>");
             gridXaml = gridXaml.Replace("Image", "Transform");
             gridXaml = gridXaml.Replace("image", "transform");
+            gridXaml = gridXaml.Replace("GUID", Guid.NewGuid().ToString());
             StringReader stringReader = new StringReader(gridXaml);
             XmlReader xmlReader = XmlReader.Create(stringReader);
             FrameworkElement newModel = (FrameworkElement)XamlReader.Load(xmlReader);
             return newModel;
-        }
-
-
-        private void imageCombo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            myImageName = e.AddedItems[0] as string;
-            SetImage();
-            RegistryKey k= Registry.CurrentUser.CreateSubKey("Software\\Terry\\PanBrowser");
-            k.SetValue("image", myImageName);
-        }
-
-        private void TransformCombo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            myTransformName = e.AddedItems[0] as string;
-            SetImage();
-            RegistryKey k = Registry.CurrentUser.CreateSubKey("Software\\Terry\\PanBrowser");
-            k.SetValue("transform", myTransformName);
         }
 
         public void SetImage()
@@ -190,17 +218,65 @@ namespace Terry
             {
                 if (myImageName == null)
                     return;
-
-                List<SliderAttribute> sliders = DoSliders(myImageName, myAttributes[0]);
-                myImageFn = PanWrapper.GetImageFunction(myImageName, sliders);
-
-                if (myTransformName != PanWrapper.None)
+                else if (myImageName == "Brill3D" || myImageName.StartsWith("Pan3D"))
                 {
-                    sliders = DoSliders(myTransformName, myAttributes[1]);
-                    myImageFn = PanWrapper.GetTransformFunction(myTransformName, myImageFn, sliders);
+                    if (brill == null)
+                    {
+                        brill = new BrillWpf.BrillDockPanel();
+                        _grid.Children.Add(brill);
+                        Grid.SetColumn(brill, 1);
+                    }
+                    brill.Visibility = Visibility.Visible;
+
+                    if (myImageName.StartsWith("Pan3D"))
+                    {
+                        List<SliderAttribute> sliders = DoSliders(myImageName, myAttributes[0]);
+                        Converter<Vector3D, double> panFn = (FastFunc<Vector3D, double>)panWrapper.GetImageFunction(myImageName, sliders);
+                        Sampler sampler = new Sampler(panFn);
+                        sampler.Resolution = 10;
+                        sampler.OnTimerTick(10);
+                        Flags flags = new Flags();
+                        flags.ShowFaces = true;
+                        sampler.Calculate(flags);
+                        CubeRenderer renderer = new CubeRenderer();
+                        RenderData r = renderer.Render(sampler, flags);
+                        brill.SetGeometry(r.Mesh);
+
+                    }
+                    else if (myImageName == "Brill3D")
+                    {
+                        if (brillRenderer == null)
+                        {
+                            brillRenderer = new BrillRenderer();
+                            //brillRenderer.Storyboard = _storyboard;
+                            brillRenderer.Init();
+                        }
+                        brill.Fill(brillRenderer);
+                    }
+                    myImageFn = (FastFunc<Pan.Point, Pan.Color>)panWrapper.GetImageFunction("Pan.wavyRings", new List<SliderAttribute>());
+                }
+                else
+                {
+                    if(brill!=null)
+                        brill.Visibility = Visibility.Hidden;
+
+                    List<SliderAttribute> sliders = DoSliders(myImageName, myAttributes[0]);
+                    myImageFn = (FastFunc<Pan.Point, Pan.Color>)panWrapper.GetImageFunction(myImageName, sliders);
+                    myImageFn = (FastFunc<Pan.Point, Pan.Color>)panWrapper.StandardTransforms(
+                        myImageFn, imageSizeSlider.Value, imageRotateSlider.Value*36, imageXSlider.Value, imageYSlider.Value);
+
+                    if (myTransformName != panWrapper.None && !string.IsNullOrEmpty(myTransformName))
+                    {
+                        sliders = DoSliders(myTransformName, myAttributes[1]);
+                        myImageFn = panWrapper.GetTransformFunction(myTransformName, myImageFn, sliders);
+                        myImageFn = (FastFunc<Pan.Point, Pan.Color>)panWrapper.StandardTransforms(
+                            myImageFn, transformStandardSliders[0].Value, transformStandardSliders[1].Value * 36,
+                            -transformStandardSliders[2].Value, -transformStandardSliders[3].Value);
+                    }
+
+                    Recalculate();
                 }
 
-                Recalculate();
             }
             catch(Exception)
             {}
@@ -217,12 +293,11 @@ namespace Terry
         {
             List<SliderAttribute> sliders = new List<SliderAttribute>();
 
-            var ret = PanWrapper.GetSliders(functionName);
+            var ret = panWrapper.GetSliders(functionName);
 
             List<string> toDelete = new List<string>();
             foreach (SliderAttribute a in attributes)
-                if (a != sizeSlider)
-                    toDelete.Add(a.Name);
+                toDelete.Add(a.Name);
 
             foreach (SliderAttribute a in ret)
             {
@@ -239,14 +314,37 @@ namespace Terry
             return sliders;
         }
 
+        private void imageCombo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            myImageName = e.AddedItems[0] as string;
+            minstep = 4;    //reset it
+            SetImage();
+            RegistryKey k = Registry.CurrentUser.CreateSubKey("Software\\Terry\\PanBrowser");
+            k.SetValue("image", myImageName);
+        }
+
+        private void TransformCombo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            myTransformName = e.AddedItems[0] as string;
+            minstep = 4;    //reset it
+            SetImage();
+            RegistryKey k = Registry.CurrentUser.CreateSubKey("Software\\Terry\\PanBrowser");
+            k.SetValue("transform", myTransformName);
+        }
+
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            SetImage();
+            //SetImage();
+            minstep = 4;    //reset it
             Recalculate();
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            if ((string)((Slider)sender).Tag == "Size" && brill != null)
+            {
+                brill.SetSize(e.NewValue);
+            }
             SetImage();
             Recalculate();
         }
@@ -273,7 +371,45 @@ namespace Terry
             }
         }
 
+        private void Window_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            //if (sizeSliderControl==null)
+            //    sizeSliderControl = VisualTreeHelperExtensions.FindDescendent<Slider>(imageSliderList);
+            imageSizeSlider.Value *= Math.Pow( 1.1, e.Delta / 100);
 
+        }
+
+        private void PlayButton3D_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            object tag = ((UserControl)e.Source).Tag;
+            if (tag is Storyboard)
+            {
+                Storyboard story = tag as Storyboard;
+                if(story.GetCurrentState()==ClockState.Active)
+                    story.Stop();
+                else
+                    story.Begin();
+            }
+            else
+            {
+                Slider s = ((Panel)((UserControl)e.Source).Parent).FindName("Slider") as Slider;
+                DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+                myDoubleAnimation.From = s.Minimum;
+                myDoubleAnimation.To = s.Maximum;
+                myDoubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(10));
+                myDoubleAnimation.AccelerationRatio = 0.2;
+                myDoubleAnimation.DecelerationRatio = 0.2;
+                myDoubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+                myDoubleAnimation.AutoReverse = true;
+                Storyboard.SetTarget(myDoubleAnimation, s);
+                Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(Slider.ValueProperty));
+
+                Storyboard myStoryboard = new Storyboard();
+                myStoryboard.Children.Add(myDoubleAnimation);
+                myStoryboard.Begin();
+                ((UserControl)e.Source).Tag = myStoryboard;
+            }
+        }
 
     }
 }
